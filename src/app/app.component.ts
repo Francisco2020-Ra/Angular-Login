ece3d5139a6ae329ca7289cb9cc4b3cbeade882e
Francisco2020-Ra <franciscorafaelromero@gmail.com>
2022-10-20 14:00:23 -0300

    Configurando firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import firebase from 'firebase/compat/app'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    firebase.initializeApp({
+      apiKey: "AIzaSyB_7JdO5AQ5o86nujSKUaSY-qyjfgxcoEU",
+      authDomain: "mis-clientes-9b97e.firebaseapp.com",
+
+    })
+  }
   title = 'login';
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
+import { NgFor } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +12,14 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  login(form:NgForm){
+
+    const email = form.value.email;
+
+    const password = form.value.password;
+
   }
 
 }
